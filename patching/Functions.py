9fbb6d219da2152e6a27e519d6eaba47548f854d
Joseph <100003036+josephanimate2021@users.noreply.github.com>
2026-02-14 21:50:05 -0500

    Remove warp_to_start condition from asm_files
    Removed condition for appending 'warp_to_start.yaml' based on patch_data options.

--- patching/Functions.py
+++ patching/Functions.py
@@ -60,8 +60,6 @@ def get_asm_files(patch_data):
         asm_files.append("asm/conditional/qol_mermaid_suit.yaml")
     if patch_data["options"]["goal"] == OracleOfAgesGoal.option_beat_ganon:
         asm_files.append("asm/conditional/ganon_goal.yaml")
-    if patch_data["options"]["warp_to_start"]:
-        asm_files.append("asm/conditional/warp_to_start.yaml")
     if patch_data["options"]["lynna_gardener"]:
         asm_files.append("asm/conditional/lynna_gardener.yaml")
     return asm_files
