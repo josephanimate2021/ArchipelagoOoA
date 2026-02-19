70749bda77c00129a7261c23a2f28b59ba00b356
Joseph <100003036+josephanimate2021@users.noreply.github.com>
2026-02-19 10:08:55 -0500

    Update Functions.py

--- patching/Functions.py
+++ patching/Functions.py
@@ -187,12 +187,8 @@ def write_chest_contents(rom: RomData, patch_data):
         ) != COLLECT_CHEST and not location_data.get(
             "is_chest", False
         ) and location_name != "Bush Cave Chest") or (
-            not patch_data['options']['secret_locations'] and (
-                (
-                    "dungeon" in location_data and location_data["dungeon"] == 11
-                ) or "secret_location" in location_data
-            )
-        ) or ("dontOverwriteChestData" in location_data and location_data["dontOverwriteChestData"] is True):
+            not patch_data['options']['heros_cave'] and "dungeon" in location_data and location_data["dungeon"] == 11
+        ):
             continue
         if location_name == "Nuun Highlands Cave":
             chest_addr = rom.get_chest_addr(location_data['room'][patch_data["options"]["animal_companion"]])
