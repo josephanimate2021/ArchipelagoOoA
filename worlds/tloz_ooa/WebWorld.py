7d8ba07d6a8f6118b223f158522737fb7b27c8ef
SenPierre <seneclauze.pierre@gmail.com>
2026-05-05 20:05:25 +0200

    Trying to fix a weird bug with the autotest

--- worlds/tloz_ooa/WebWorld.py
+++ worlds/tloz_ooa/WebWorld.py
@@ -3,11 +3,12 @@ from BaseClasses import Tutorial
 
 class OracleOfAgesWeb(WebWorld):
     theme = "grass"
-    tutorials = [Tutorial(
+    setup_en = Tutorial(
         "Multiworld Setup Guide",
         "A guide to setting up Oracle of Ages for Archipelago on your computer.",
         "English",
         "ooa_setup_en.md",
         "ooa_setup/en",
         ["SenPierre"]
-    )]
+    )
+    tutorials = [setup_en]
